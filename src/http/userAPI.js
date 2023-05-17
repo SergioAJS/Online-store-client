f3a073d18a877ac44090fb31a216b1e06e1c3446
SergioAJS <103531486+SergioAJS@users.noreply.github.com>
2023-05-17 17:46:35 +0500

    feat: add admin registration

--- src/http/userAPI.js
+++ src/http/userAPI.js
@@ -1,11 +1,21 @@
 import { $authHost, $host } from "./index";
 import jwt_decode from "jwt-decode";
 
-export const registration = async (email, password) => {
+export const registrationUser = async (email, password) => {
   const { data } = await $host.post("api/user/registration", {
     email,
     password,
     role: "USER",
+  });
+  localStorage.setItem("token", data.token);
+  return jwt_decode(data.token);
+};
+
+export const registrationAdmin = async (email, password) => {
+  const { data } = await $host.post("api/user/registration", {
+    email,
+    password,
+    role: "ADMIN",
   });
   localStorage.setItem("token", data.token);
   return jwt_decode(data.token);
