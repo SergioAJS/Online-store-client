e6a223e6a4cdb18af78e6ed59e50cb56393ffa5b
SergioAJS <103531486+SergioAJS@users.noreply.github.com>
2023-05-17 18:09:25 +0500

    feat: add lazy loading to authRoutes

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,33 @@
-import Admin from "./pages/Admin"
+import { Suspense, lazy } from 'react';
 import Auth from "./pages/Auth"
-import Basket from "./pages/Basket"
 import DevicePage from "./pages/DevicePage"
 import Shop from "./pages/Shop"
 import { ADMIN_ROUTE, DEVICE_ROUTE, LOGIN_ROUTE, SHOP_ROUTE, BASKET_ROUTE, REGISTRATION_ROUTE} from "./utils/consts"
 
+const Admin = lazy(() => import("./pages/Admin"))
+const Basket = lazy(() => import("./pages/Basket"))
+
+const AdminLazy = () => {
+    return (
+        <Suspense><Admin/></Suspense>
+    )
+}
+
+const BasketLazy = () => {
+    return (
+        <Suspense><Basket/></Suspense>
+    )
+}
+
+
 export const authRoutes  = [
     {
         path: ADMIN_ROUTE,
-        Component: Admin
+        Component: AdminLazy
     },
     {
         path: BASKET_ROUTE,
-        Component: Basket
+        Component: BasketLazy
     },
 ]
 
